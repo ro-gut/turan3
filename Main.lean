ec9309d5f2ae29e4f5c6ac125dcb7b3544971776
Rodrigo solocode <ro_solocode@MacBookAir.fritz.box>
2025-01-14 16:34:41 +0100

    Initialize Lean project with lakefile and dependencies

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import turan3
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ turan3.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `turan3` library.
+-- Import modules here that should be built as part of the library.
+import turan3.Basic
