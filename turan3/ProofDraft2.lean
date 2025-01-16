f5ad34c1a5d25067fd0a72f5d7353d2ea284f6ee
Rodrigo solocode <ro_solocode@MacBookAir.fritz.box>
2025-01-16 16:41:44 +0100

    help theorem

--- turan3/ProofDraft2.lean
+++ turan3/ProofDraft2.lean
@@ -8,8 +8,6 @@ local notation "V" => @Finset.univ α _
 local notation "E" => G.edgeFinset
 local notation "n" => Fintype.card α
 
-#eval Lean.versionString
-
 structure FunToMax where
   w : α → NNReal
   h_w : ∑ v in V, w v = 1
@@ -28,21 +26,26 @@ def SupposedMax {k : ℕ} (hk : k ≠ 0) {clique : Finset α} (hc : clique.card 
     simp only [ne_eq, Nat.cast_eq_zero] -- from simp?
     apply hk
 
-#check SimpleGraph.IsClique
-
-lemma main (W : FunToMax G) : ∃ clique_verts : Finset α, ∃ hk : clique_verts.card ≠ 0,
-  W.fw ≤ (SupposedMax G hk rfl).fw := by
-    sorry
-
-#check sSup
-#check Set.image
 
 noncomputable
 def definitionalMax := sSup (Set.image FunToMax.fw (Set.univ : Set (FunToMax G)))
 
-lemma help_1 : (Set.image FunToMax.fw (Set.univ : Set (FunToMax G))).Nonempty := by
-  sorry
+---------------
 
-lemma help_2 : BddAbove (Set.image FunToMax.fw (Set.univ : Set (FunToMax G))) := by
-  sorry
-  -- My suggestion, bia : ∀ v : V, w v ≤ 1
+w
+
+
+
+open Classical
+
+noncomputable
+def K (W : FunToMax G) := Nat.find (help G W)
+
+lemma help2 (W : FunToMax G):
+  ∃ better : FunToMax G,
+    (∀ i, W.w i = 0 → better.w i = 0) ∧ -- support is included in that of W
+    (((Finset.univ : Finset α).filter (fun i => better.w i = 0)).card = (K G W)) ∧ -- support has size m
+    (W.fw ≤ better.fw) -- has better weights
+    := Nat.find_spec (help G W)
+
+#check Nat.find_le
