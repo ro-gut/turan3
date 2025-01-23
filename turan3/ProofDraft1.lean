9c06479c4242fce4a62e95033959fa45323e3f15
Rodrigo solocode <ro_solocode@MacBookAir.fritz.box>
2025-01-23 20:37:30 +0100

    Proof Improved

--- turan3/ProofDraft1.lean
+++ turan3/ProofDraft1.lean
@@ -8,8 +8,6 @@ local notation "V" => @Finset.univ α _
 local notation "E" => G.edgeFinset
 local notation "n" => Fintype.card α
 
-open Finset SimpleGraph
-
 structure FunToMax where
   w : α → NNReal
   h_w : ∑ v in V, w v = 1
@@ -19,17 +17,60 @@ structure FunToMax where
         · apply refl
         · apply mul_comm)
 
--- Turán's Theorem
+noncomputable
+def SupposedMax {k : ℕ} (hk : k ≠ 0) {clique : Finset α} (hc : clique.card = k) : FunToMax G where
+  w := fun x => if x ∈ clique then 1/k else 0
+  h_w := by
+    rw [Finset.sum_ite,Finset.sum_const_zero, add_zero, Finset.sum_const,Finset.filter_univ_mem,hc]
+    rw [one_div, nsmul_eq_mul, mul_inv_cancel₀]
+    simp only [ne_eq, Nat.cast_eq_zero] -- from simp?
+    apply hk
+
+
+noncomputable
+def definitionalMax := sSup (Set.image FunToMax.fw (Set.univ : Set (FunToMax G)))
+
+---------------------
+
+theorem help (W : FunToMax G) : ∃ m : ℕ, (fun m =>
+  ∃ better : FunToMax G,
+    (∀ i, W.w i = 0 → better.w i = 0) ∧ -- support is included in that of W
+    (((Finset.univ : Finset α).filter (fun i => better.w i > 0)).card = m) ∧ -- support has size m
+    (W.fw ≤ better.fw) -- has better weights
+    ) m := by
+    let m := ((Finset.univ : Finset α).filter (fun i => W.w i = 0)).card
+    use m
+    let better := W
+    use better
+    have hP : ∀ (i : α), W.w i = 0 → better.w i = 0 := by
+      intro i h_w_zero
+      have better_eq : better.w = W.w := rfl
+      rw [better_eq]
+      exact h_w_zero
+    have hQ : (Finset.filter (fun i => better.w i = 0) Finset.univ).card = m := by
+      exact rfl
+    have hR : W.fw ≤ better.fw := by
+      have better_fw_eq : better.fw = W.fw := rfl
+      rw [better_fw_eq]
+    exact ⟨hP, ⟨hQ, hR⟩⟩
+
+open Classical
+
+noncomputable
+def K (W : FunToMax G) := Nat.find (help G W)
+
+lemma help2 (W : FunToMax G):
+  ∃ better : FunToMax G,
+    (∀ i, W.w i = 0 → better.w i = 0) ∧ -- support is included in that of W
+    (((Finset.univ : Finset α).filter (fun i => better.w i = 0)).card = (K G W)) ∧ -- support has size m
+    (W.fw ≤ better.fw) -- has better weights
+    := Nat.find_spec (help G W)
+
+#check Nat.find_le
+
+open Finset SimpleGraph
+
 theorem turan (h0 : p ≥ 2) (h1 : G.CliqueFree p)
   (w : α → NNReal) (h_w : ∑ v in V, w v = 1) :
   #E ≤ (1 -  1 / (p - 1)) * n^2 / 2 := by
   sorry
-
-
-
-#check E
-#check Sym2.lift
-#check Quot.liftOn
-#check G.edgeFinset
-#check @mul_comm NNReal
-#check (λ x y : NNReal => mul_comm x y)
--- turan3/ProofDraft2.lean
+++ turan3/ProofDraft2.lean
@@ -8,6 +8,8 @@ local notation "V" => @Finset.univ α _
 local notation "E" => G.edgeFinset
 local notation "n" => Fintype.card α
 
+open Finset SimpleGraph
+
 structure FunToMax where
   w : α → NNReal
   h_w : ∑ v in V, w v = 1
@@ -17,28 +19,14 @@ structure FunToMax where
         · apply refl
         · apply mul_comm)
 
-noncomputable
-def SupposedMax {k : ℕ} (hk : k ≠ 0) {clique : Finset α} (hc : clique.card = k) : FunToMax G where
-  w := fun x => if x ∈ clique then 1/k else 0
-  h_w := by
-    rw [Finset.sum_ite,Finset.sum_const_zero, add_zero, Finset.sum_const,Finset.filter_univ_mem,hc]
-    rw [one_div, nsmul_eq_mul, mul_inv_cancel₀]
-    simp only [ne_eq, Nat.cast_eq_zero] -- from simp?
-    apply hk
-
-
-noncomputable
-def definitionalMax := sSup (Set.image FunToMax.fw (Set.univ : Set (FunToMax G)))
-
----------------------
-
+-- help: Assures there exists an m s.t there is a weight function "better" with the following properties
 theorem help (W : FunToMax G) : ∃ m : ℕ, (fun m =>
   ∃ better : FunToMax G,
     (∀ i, W.w i = 0 → better.w i = 0) ∧ -- support is included in that of W
-    (((Finset.univ : Finset α).filter (fun i => better.w i = 0)).card = m) ∧ -- support has size m
+    (((Finset.univ : Finset α).filter (fun i => better.w i > 0)).card = m) ∧ -- support has size m
     (W.fw ≤ better.fw) -- has better weights
     ) m := by
-    let m := ((Finset.univ : Finset α).filter (fun i => W.w i = 0)).card
+    let m := ((Finset.univ : Finset α).filter (fun i => W.w i > 0)).card
     use m
     let better := W
     use better
@@ -47,7 +35,7 @@ theorem help (W : FunToMax G) : ∃ m : ℕ, (fun m =>
       have better_eq : better.w = W.w := rfl
       rw [better_eq]
       exact h_w_zero
-    have hQ : (Finset.filter (fun i => better.w i = 0) Finset.univ).card = m := by
+    have hQ : (Finset.filter (fun i => better.w i > 0) Finset.univ).card = m := by
       exact rfl
     have hR : W.fw ≤ better.fw := by
       have better_fw_eq : better.fw = W.fw := rfl
@@ -57,18 +45,139 @@ theorem help (W : FunToMax G) : ∃ m : ℕ, (fun m =>
 open Classical
 
 noncomputable
+-- With help G W, K represents the smallest size (m) of the weight support
+-- Computes the smallest possible m
 def K (W : FunToMax G) := Nat.find (help G W)
 
+-- Pinpoints size m = K(G, W), ensures that the weight function better achieves the optimal size K
 lemma help2 (W : FunToMax G):
   ∃ better : FunToMax G,
     (∀ i, W.w i = 0 → better.w i = 0) ∧ -- support is included in that of W
-    (((Finset.univ : Finset α).filter (fun i => better.w i = 0)).card = (K G W)) ∧ -- support has size m
+    (((Finset.univ : Finset α).filter (fun i => better.w i > 0)).card = (K G W)) ∧ -- support has size m
     (W.fw ≤ better.fw) -- has better weights
     := Nat.find_spec (help G W)
 
-#check Nat.find_le
+-- Define optimal weight function with size m = K(G,W)
+noncomputable
+def Better (W : FunToMax G) : FunToMax G := Classical.choose (help2 G W)
 
-open Finset SimpleGraph
+-- Ensures Better's support is included in W's support
+lemma BetterSupportIncluded (W : FunToMax G)  (i : α) (hi : W.w i = 0) : (Better G W).w i = 0 :=
+  (Classical.choose_spec (help2 G W)).1 i hi
+
+-- Confirms the size of Better's support (K(G,W)) being the smallest size guaranteed by help2
+lemma BetterSupportSize (W : FunToMax G) : ((Finset.univ : Finset α).filter (fun i => (Better G W).w i > 0)).card = (K G W) :=
+  (Classical.choose_spec (help2 G W)).2.1
+
+-- Ensures that the function of Better is at least as large as that of W
+lemma BetterHigher (W : FunToMax G) : W.fw ≤ (Better G W).fw :=
+  (Classical.choose_spec (help2 G W)).2.2
+
+-- Define a new weight function redistributing weight from one vertex (loose) to another (gain)
+-- Notes : I added h_neq as an assumption
+def Improve (W : FunToMax G) (loose gain : α) (h_neq : gain  ≠ loose) : FunToMax G where
+  w := fun i =>
+          if i = loose
+          then 0
+          else if i = gain
+               then W.w gain + W.w loose
+               else W.w i
+  h_w := by
+    have remember := W.h_w
+    rw [sum_ite]
+    simp
+    --⊢ (∑ x ∈ filter (fun x => ¬x = loose) univ, if x = gain then W.w gain + W.w loose else W.w x) = 1
+    -- sum of           all x =! loose           ,        condition          = 1
+    rw[Finset.sum_ite]
+    have : filter (fun x => x = gain) (filter (fun x => ¬x = loose) univ) = {gain} := by
+      rw[Finset.filter_filter]
+      ext a
+      constructor
+      · intro h
+        rw[Finset.mem_filter] at h
+        simp only [Finset.mem_singleton]
+        exact h.2.2
+      · intro h
+        simp only [Finset.mem_singleton] at h
+        rw[Finset.mem_filter]
+        constructor
+        · exact Finset.mem_univ a
+        · constructor
+          · intro contra
+            rw[h] at contra
+            exact h_neq contra
+          · exact h
+    -- Rewrite using previous result
+    rw[this]
+    rw[Finset.sum_singleton]
+    rw[Finset.filter_filter]
+    -- S is set of vertices excluding gain and loose
+    let S := filter (fun x => x ≠ gain ∧ x ≠ loose) univ
+    -- Rewrite total sum of weights in terms of gain, loose and S
+    have h_sum : ∑ x ∈ univ, W.w x = (W.w gain + W.w loose) + ∑ x ∈ S, W.w x := by
+      rw[←Finset.sum_add_sum_compl (filter (fun x => x = gain ∨ x = loose) univ)]
+      rw[Finset.filter_or]
+      rw[Finset.sum_union]
+      ·
+        have gain_filter : filter (fun x => x = gain) univ = {gain} := by
+          ext x
+          simp[Finset.mem_filter, Finset.mem_univ]
+        have loose_filter : filter (fun x => x = loose) univ = {loose} := by
+          ext x
+          simp[Finset.mem_filter, Finset.mem_univ]
+        rw[gain_filter]
+        rw[loose_filter]
+        rw[Finset.sum_singleton]
+        rw[Finset.sum_singleton]
+        -- Proof that complement of {gain} AND {loose} is S
+        have compl_eq : ({gain} ∪ {loose})ᶜ = S := by
+          ext x
+          simp [Finset.mem_compl, Finset.mem_union, Finset.mem_singleton, S]
+        rw[compl_eq]
+      -- Proof {gain} and {loose} are disjoint
+      · rw[Finset.disjoint_left]
+        intros x hx h'x
+        rw[Finset.mem_filter] at hx h'x
+        have contra : gain = loose := by
+          rw[←hx.2, h'x.2]
+        exact h_neq contra
+    have filter_eq_S : filter (fun a => ¬a = loose ∧ ¬a = gain) univ = S := by
+      ext x
+      simp[Finset.mem_filter]
+      constructor
+      · intro h
+        rw [Finset.mem_filter]
+        exact ⟨Finset.mem_univ x, ⟨h.2, h.1⟩⟩
+      · intro h
+        rw [Finset.mem_filter] at h
+        exact ⟨h.2.2, h.2.1⟩
+    rw[filter_eq_S]
+    rw[←h_sum]
+    rw[remember]
+
+
+lemma Improve_does_its_thing (W : FunToMax G) (loose gain : α) (h : W.w gain ≥ W.w loose) (h_neq : gain ≠ loose):
+  (Improve G W loose gain h_neq).fw ≥ W.fw := by
+  simp only [FunToMax.fw, Improve]
+  sorry
+
+
+
+lemma ImproveReducesSupport (W : FunToMax G) (loose gain : α) :
+  ∀ i, W.w i = 0 → (Improve G W loose gain).w i = 0 := by
+  sorry
+
+
+lemma BetterFormsClique (W : FunToMax G) : G.IsClique ((Finset.univ : Finset α).filter (fun i => (Better G W).w i > 0)) := by
+  by_contra con
+  dsimp [IsClique, Set.Pairwise] at con
+  push_neg at con
+  obtain ⟨x,xdef,y,ydef,xny,xyAdj⟩ := con
+  wlog wlog : (Better G W).w x ≤ (Better G W).w y with SymCase
+  · push_neg at wlog
+    specialize SymCase G W y ydef x xdef (ne_comm.mp xny) -- ...
+    sorry
+  · sorry
 
 theorem turan (h0 : p ≥ 2) (h1 : G.CliqueFree p)
   (w : α → NNReal) (h_w : ∑ v in V, w v = 1) :
