d9e8fc159761b7922e8b9a84696cf2078ac5f0c8
ro-gut <78649273+ro-gut@users.noreply.github.com>
2025-04-24 12:01:28 +0200

    copyright

--- turan3/ProofDraft2.lean
+++ turan3/ProofDraft2.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Rodrigo Gutierrez, Yves Jäckle
+-/
+
 import Mathlib.Topology.Basic
 
 import Mathlib
