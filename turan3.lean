271198efb4a0f6a101fb2e075103c60415a5baac
Rodrigo solocode <ro_solocode@MacBookAir.fritz.box>
2025-08-17 18:42:00 +0200

    Fix import: Turan_3rdProof → Turans_3rdProof

--- turan3.lean
+++ turan3.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `turan3` library.
 -- Import modules here that should be built as part of the library.
 
-import turan3.Turan_3rdProof
+import turan3.Turans_3rdProof
