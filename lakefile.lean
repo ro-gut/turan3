cc4f6dfcb64de8c6c2960f0e7e3e0a7605ef0af6
ro_solocode <ro_solocode@MacBookAir.fritz.box>
2025-08-19 10:52:25 +0200

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -7,3 +7,9 @@ require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "m
 
 @[default_target]
 lean_lib turan3
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
