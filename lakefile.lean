b8d935aae86f753fae0ea9fd930539c8b0e2c5b1
Rodrigo solocode <ro_solocode@MacBookAir.fritz.box>
2025-09-15 20:13:42 +0200

    Update Lean 4.24 (working)

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,8 @@ open Lake DSL
 
 package turan3
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+require mathlib from
+  git "https://github.com/leanprover-community/mathlib4"
 
 @[default_target]
 lean_lib turan3
